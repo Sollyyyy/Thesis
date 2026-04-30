65ef7b15d2c8abb0a6e10d874f3c70a0b3433449
Zeyad Soliman <zeyadsoliman16@gmail.com>
2026-04-30 16:43:58 +0200

    updated test_database.py

--- backend/tests/test_database.py
+++ backend/tests/test_database.py
@@ -1,1 +1,23 @@
-import database
+from database import get_user, create_user, delete_user
+from argon2 import PasswordHasher
+
+
+def test_get_user():
+    user = get_user('user1')
+    assert user.username == 'user1'
+    assert user.email == 'user1@gmail.com'
+
+
+def test_create_user():
+    ph = PasswordHasher()
+    create_user('usertest', 'usertest@gmail.com', 'usertest', ph.hash('password'), 'user')
+    user = get_user('usertest')
+    assert user.username == 'usertest'
+    assert user.email == 'usertest@gmail.com'
+
+
+def test_delete_user():
+    delete_user('usertest')
+    user = get_user('usertest')
+    assert user is None
+
